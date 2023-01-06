82f317ec64cef2d3eebe3571c632a39f6a6b6cb3
AKRA <timur.akra@outlook.com>
2023-01-06 17:37:47 +0300

    add new solution _Length_of_Last_Word

--- 58. Length of Last Word/Program.cs
+++ 58. Length of Last Word/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Solution.LengthOfLastWord("Hello, World"));
         }
     }
 }
---
+++ 58. Length of Last Word/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _58._Length_of_Last_Word
+{
+    public class Solution
+    {
+        public static int LengthOfLastWord(string s)
+        {
+            var res = s.Trim().Split();
+            return res[res.Length -1].Length;
+        }
+    }
+}
