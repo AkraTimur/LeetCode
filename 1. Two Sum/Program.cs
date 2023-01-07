c9a347d1cec1456c7d1b84276cc88e399a840975
AKRA <timur.akra@outlook.com>
2023-01-07 19:37:28 +0300

    add new solution _1._Two_Sum

---
+++ 1. Two Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _1._Two_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Solution.TwoSum(new[] { 3, 2, 4 }, 6);
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ 1. Two Sum/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._Two_Sum
+{
+    public  class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var list = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        list.Add(i);
+                        list.Add(j);
+                        break;
+                    }
+                }
+            }
+
+            return list.ToArray();
+        }
+    }
+}
