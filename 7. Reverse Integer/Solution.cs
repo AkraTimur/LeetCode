0d9154b75b61be9028659989b764ae183b613c77
AKRA <timur.akra@outlook.com>
2023-01-13 21:39:56 +0300

    add new solution

---
+++ 7. Reverse Integer/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._Reverse_Integer
+{
+    public class Solution
+    {
+        public static int Reverse(int x)
+        {
+            var str = $"{x}";
+            var reverseStr = str.Reverse();
+            string res = new string(reverseStr.ToArray());
+            int number = Convert.ToInt32(res);
+            
+            
+            return number;
+        }
+    }
+}
