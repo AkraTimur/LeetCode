3d17aca56c521d2deee2ae05f340d3c0a952e1ed
AKRA <99009898+AkraTimur@users.noreply.github.com>
2023-01-21 17:31:01 +0300

    add new solution

---
+++ 69. Sqrt(x)/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace _69._Sqrt_x_
+{
+    public static class Solution
+    {
+        public static int MySqrt(int x)
+        {
+            double approx = (double)x / 2;
+            double error = double.Epsilon;
+
+            while (Math.Abs(approx * approx - x) > error)
+            {
+                approx = (approx + (double)x / approx) / 2;
+            }
+            return (int)approx;
+        }
+    }
+}
