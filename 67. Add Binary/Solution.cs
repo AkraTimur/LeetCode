4ee8d74c778e5a642bf8f58422da13ab6dd6c249
AKRA <99009898+AkraTimur@users.noreply.github.com>
2023-01-21 14:03:12 +0300

    update

--- 67. Add Binary/Solution.cs
+++ 67. Add Binary/Solution.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _67._Add_Binary
+﻿namespace _67._Add_Binary
 {
     public static class Solution
     {
