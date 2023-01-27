cac28bec490692b04feb0e98e5e6b63744242575
AKRA <99009898+AkraTimur@users.noreply.github.com>
2023-01-27 18:05:07 +0300

    add new solution (not finish)

---
+++ 989. Add to Array-Form of Integer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _989._Add_to_Array_Form_of_Integer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 989. Add to Array-Form of Integer/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _989._Add_to_Array_Form_of_Integer
+{
+    public static class Solution
+    {
+        public static IList<int> AddToArrayForm(int[] num, int k)
+        {
+            string str = string.Join("", num);
+            var list = new List<int>();
+            list.Add(int.Parse(str) + k);
+
+            return list;
+        }
+    }
+}
+//https://leetcode.com/problems/add-to-array-form-of-integer/
