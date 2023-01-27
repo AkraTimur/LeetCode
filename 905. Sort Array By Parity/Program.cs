dd0b2c0ab02265d1f254f47041efbb6b74524b8f
AKRA <99009898+AkraTimur@users.noreply.github.com>
2023-01-27 17:48:39 +0300

    add new solution

---
+++ 905. Sort Array By Parity/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _905._Sort_Array_By_Parity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.SortArrayByParity(new int[] { 3, 1, 2, 4 }));
+        }
+    }
+}
