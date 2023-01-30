b80460a77f020251bc3cc83aa110db53af8f117f
AKRA <99009898+AkraTimur@users.noreply.github.com>
2023-01-30 10:12:15 +0300

    add new solution

---
+++ 1365. How Many Numbers Are Smaller Than the Current Number/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1365._How_Many_Numbers_Are_Smaller_Than_the_Current_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
