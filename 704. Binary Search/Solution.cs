039bfdf8d61b3ca8320d88c8ff287046d22dcda2
AKRA <99009898+AkraTimur@users.noreply.github.com>
2023-01-31 18:56:58 +0300

    add new solution

---
+++ 704. Binary Search/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace _704._Binary_Search
+{
+    public class Solution
+    {
+        public static int Search(int[] nums, int target)
+        {
+            var low = 0;
+            var high = nums.Length - 1;
+            var guess = 0;
+            int count = 0;
+
+            while (low <= high)
+            {
+                guess = (low + high) / 2;
+                if (target == nums[guess]) return guess;
+                else if(target < nums[guess])
+                {
+                    high = guess - 1;
+                }
+                else
+                {
+                    low= guess + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
+
+
