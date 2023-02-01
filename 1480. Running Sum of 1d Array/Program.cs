d5371f20634908142acee30429aa799ba0e2afa6
AKRA <99009898+AkraTimur@users.noreply.github.com>
2023-02-01 14:28:57 +0300

    add new solution

---
+++ 1480. Running Sum of 1d Array/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1480._Running_Sum_of_1d_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.RunningSum(new int[] { 1, 1, 1, 1, 1 }));
+        }
+    }
+}
