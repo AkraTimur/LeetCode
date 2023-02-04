eece7f7d79fd33667c5d2e321f89bb04675e4909
AKRA <timur.akra@outlook.com>
2023-02-04 20:04:32 +0300

    add new solution

--- 367. Valid Perfect Square/Program.cs
+++ 367. Valid Perfect Square/Program.cs
@@ -1,21 +1,29 @@
 ﻿namespace _367._Valid_Perfect_Square
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(IsPerfectSquare(16));
+            Console.WriteLine(IsPerfectSquare(808201));
         }
 
         public static bool IsPerfectSquare(int num)
         {
-            int lowNum = 0, highNum = num/2;
-            if ((num / 2) * (num / 2) == num) return true;
+            int lowNum = 1, highNum = num/2;
+            if ((num / 2) * (num / 2) == num || num == 1) return true;
 
             while(lowNum <= highNum)
             {
                 int mid = lowNum + (highNum - lowNum) / 2;
-                if
+                if (mid * mid == num) return true;
+                else if(mid * mid < num)
+                {
+                    lowNum = mid +1;
+                }
+                else
+                {
+                    highNum = mid - 1;
+                }
             }
 
             return false;
