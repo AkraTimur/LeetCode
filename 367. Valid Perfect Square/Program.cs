655ed91c88f306e0aa4e1f90a595cf27309a107d
AKRA <99009898+AkraTimur@users.noreply.github.com>
2023-02-04 12:41:34 +0300

    new solve/ not finish

---
+++ 367. Valid Perfect Square/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _367._Valid_Perfect_Square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPerfectSquare(16));
+        }
+
+        public static bool IsPerfectSquare(int num)
+        {
+            int lowNum = 0, highNum = num/2;
+            if ((num / 2) * (num / 2) == num) return true;
+
+            while(lowNum <= highNum)
+            {
+                int mid = lowNum + (highNum - lowNum) / 2;
+                if
+            }
+
+            return false;
+        }
+    }
+}
