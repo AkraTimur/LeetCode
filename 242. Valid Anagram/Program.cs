a86ba76ba98aa21cf8204c72d761a49e7c645a9e
AKRA <AkraTimur@users.noreply.github.com>
2023-07-04 09:23:46 +0300

    add new solution

---
+++ 242. Valid Anagram/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _242._Valid_Anagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "anagram", t = "nagaram";
+
+            Console.WriteLine(Solution.IsAnagram(s, t));
+        }
+
+        public class Solution
+        {
+            public static bool IsAnagram(string s, string t)
+            {
+                if(s.Length != t.Length) return false;
+
+                var arrS = s.ToCharArray();
+                var arrT = t.ToCharArray();
+
+                Array.Sort(arrS);
+                Array.Sort(arrT);
+
+                for (int i = 0;i < arrS.Length; i++)
+                {
+                    if (arrS[i] != arrT[i]) return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
