ad963a12e4ddc1e0969e079645a0c96931c87616
AKRA <AkraTimur@users.noreply.github.com>
2023-07-07 09:44:25 +0300

    add new solution

---
+++ 66. Plus One/66. Plus One/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+
+namespace _66._Plus_One
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] digits = { 1, 2, 3};
+
+            var res = Solution.PlusOne(digits);
+        }
+
+        public class Solution
+        {
+            public static int[] PlusOne(int[] digits)
+            {
+                BigInteger bigInteger = BigInteger.Parse(string.Join("", digits)) + 1;
+
+                var sum = bigInteger.ToString();
+
+                List<int> result = new List<int>();
+
+                foreach (var digit in sum) 
+                {
+                    result.Add(Convert.ToInt32(digit.ToString()));
+                }
+
+                return result.ToArray();
+            }
+        }
+    }
+}
