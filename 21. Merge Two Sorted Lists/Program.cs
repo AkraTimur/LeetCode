15cf257bea6f4f933c0e7fe56a8e29fb8a4adcc9
AKRA <AkraTimur@users.noreply.github.com>
2023-08-13 07:38:27 +0300

    add new solution MergeTwoLists

---
+++ 21. Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,30 @@
+﻿using _21._Merge_Two_Sorted_Lists.Entities;
+
+namespace _21._Merge_Two_Sorted_Lists
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+            ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+
+            var res = Solution.MergeTwoLists(list1, list2);
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public static class Solution
+    {
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            //todo
+
+            var result = list1.
+
+            return new ListNode();
+        }
+    }
+}
