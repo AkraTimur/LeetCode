abd056eaae245018f3e8b98d49ac06bae33235e4
AKRA <AkraTimur@users.noreply.github.com>
2023-08-23 15:32:17 +0300

    add new kata

---
+++ 268. Missing Number/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _268._Missing_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution.MissingNumber(new int[] {1,2 }));
+        }
+
+        public class Solution
+        {
+            public static int MissingNumber(int[] nums)
+            {
+                Array.Sort(nums);
+
+                for (int i = 0; i < nums[nums.Length - 1]; i++)
+                {
+                    if (nums[i] != i)
+                    {
+                       return i;
+                    }
+                }
+
+                return nums[nums.Length - 1] += 1;
+            }
+        }
+    }
+}
