4d7e56945b1137eca6886b06e2ae4f8b17952942
Акра Тимур <AKRA@iMac-Alia.local>
2023-08-28 12:34:03 +0300

    add new solution

---
+++ 338. Counting Bits/338. Counting Bits/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _338._Counting_Bits;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var solution = Solution.CountBits(2);
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static class Solution
+    {
+        public static int[] CountBits(int n)
+        {
+            var arr = new int[n + 1];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = arr[i >> 1] + i % 2;
+            }
+
+            return arr;
+        }
+    }
+}
