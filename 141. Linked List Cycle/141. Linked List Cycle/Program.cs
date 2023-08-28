79b840285f7920f836981476a771a287a3cdf8a7
Акра Тимур <AKRA@iMac-Alia.local>
2023-08-28 15:41:38 +0300

    add new kata 141

---
+++ 141. Linked List Cycle/141. Linked List Cycle/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _141._Linked_List_Cycle;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public  class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x)
+    {
+        val = x;
+        next = null;
+    }
+}
+
+public static class Solution
+{
+    public static bool HasCycle(ListNode head)
+    {
+        if (head == null) return false;
+
+        var slow = head;
+        var fast = head;
+
+        while (fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) return true;
+        }
+
+        return false;
+    }
+}
+
