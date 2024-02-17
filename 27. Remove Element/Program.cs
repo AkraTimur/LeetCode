cb8775d2b17b67268d78d9e23d9b98a2d03a8703
AKRA <timur.akra@gmail.com>
2024-02-17 09:31:33 +0300

    add new solution

--- 27. Remove Element/Program.cs
+++ 27. Remove Element/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace _27._Remove_Element
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Solution.RemoveElement(new int[] { 3, 2, 2, 3 }, 3));
+            var sol = new Solution();
+
+            Console.WriteLine(sol.RemoveElement(new int[] { 3, 2, 2, 3 }, 3));
+        }
+    }
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+             int writePointer = 0;
+
+            foreach (int i in nums)
+            {
+                if (i != val)
+                {
+                    nums[writePointer] = i;
+                    writePointer++;
+                }
+            }
+
+            return writePointer;
         }
     }
 }
