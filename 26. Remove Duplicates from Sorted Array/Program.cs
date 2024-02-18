b0ba097da5a3b4d63bf7bf0c72b2ba54d5890da8
AKRA <timur.akra@gmail.com>
2024-02-18 22:23:05 +0300

    add solution n26

---
+++ 26. Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _26._Remove_Duplicates_from_Sorted_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution res = new Solution();
+    
+            Console.WriteLine(res.RemoveDuplicates(new[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }));
+        }
+
+        public class Solution
+        {
+            public int RemoveDuplicates(int[] nums)
+            {
+                int val = -1000;
+                int writePointer = 1;
+
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    if (nums[i] != nums[i-1])
+                    {
+                        nums[writePointer] = nums[i];
+                        writePointer++;
+                    }
+                }
+
+                return writePointer;
+            }
+        }
+    }
+}
